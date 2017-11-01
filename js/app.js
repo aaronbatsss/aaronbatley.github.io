d11c4ae81520ca7749b31e77d9652d1ccd563ed3
Aaron Batley <aaronbatsss@gmail.com>
2017-11-01 14:53:45 -0400

    <a> color change

--- js/app.js
+++ js/app.js
@@ -24,7 +24,7 @@ $(function () {
   function switchBack() {
     $('.blue').css('background-color', 'rgb(36, 148, 201');
     $('.orange').css('background-color', 'rgb(226, 94, 44');
-    $('a').css('color', 'rgba(255, 255, 255, 0.94')
+    $('a').css('color', 'rgb(66, 192, 252)')
     $('h1').css('color', 'white');
     $('p').css('color', 'white');
 
